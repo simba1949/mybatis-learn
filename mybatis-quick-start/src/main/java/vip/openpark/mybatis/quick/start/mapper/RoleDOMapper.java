e1acc129967c63a15b9c2594e361910c6cb7adf5
anthony <anthony1949@zto.com>
2023-11-07 12:39:59 +0800

    style：优化代码结构

--- mybatis-quick-start/src/main/java/vip/openpark/mybatis/quick/start/mapper/RoleDOMapper.java
+++ mybatis-quick-start/src/main/java/vip/openpark/mybatis/quick/start/mapper/RoleDOMapper.java
@@ -1,6 +1,5 @@
 package vip.openpark.mybatis.quick.start.mapper;
 
-
 import vip.openpark.infrastructure.domain.RoleDO;
 
 import java.util.List;
@@ -10,6 +9,5 @@ import java.util.List;
  * @version 2018/9/5 16:48
  */
 public interface RoleDOMapper {
-
     List<RoleDO> findAll();
 }
