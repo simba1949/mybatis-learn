936c43a4988951006e6285cd39a4953ef4d4fad5
anthony <anthony1949@zto.com>
2023-11-07 12:32:31 +0800

    build：添加 infrastructure 模块

---
+++ infrastructure/src/main/java/vip/openpark/infrastructure/domain/RoleDO.java
@@ -0,0 +1,33 @@
+package vip.openpark.infrastructure.domain;
+
+import lombok.Data;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+/**
+ * @author anthony
+ * @version 2018/9/5 16:48
+ */
+@Data
+public class RoleDO implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 7454240971789116908L;
+
+    private Long id;
+    private String code;
+    private String roleName;
+
+    private Byte blEnable;
+    private Byte blDelete;
+    private Long version;
+    private LocalDateTime gmtCreate;
+    private String creator;
+    private Long creatorId;
+    private String creatorCode;
+    private LocalDateTime gmtModifier;
+    private String modifier;
+    private Long modifierId;
+    private String modifierCode;
+}
