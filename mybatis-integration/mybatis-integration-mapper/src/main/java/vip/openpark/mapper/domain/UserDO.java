9c3baa3b9c74b2f55b52bc2415cb968153c472ac
anthony <anthony1949@zto.com>
2023-11-13 11:50:54 +0800

    feature：mybatis 集成通用 mapper 提交测试类

--- mybatis-integration/mybatis-integration-mapper/src/main/java/vip/openpark/mapper/domain/UserDO.java
+++ mybatis-integration/mybatis-integration-mapper/src/main/java/vip/openpark/mapper/domain/UserDO.java
@@ -37,13 +37,13 @@ public class UserDO implements Serializable {
      * 真实姓名
      */
     @Column(name = "real_name")
-    private String real_name;
+    private String realName;
 
     /**
      * 用户昵称
      */
     @Column(name = "nick_name")
-    private String nick_name;
+    private String nickName;
 
     /**
      * 密码
@@ -73,13 +73,13 @@ public class UserDO implements Serializable {
      * 国家
      */
     @Column(name = "country_name")
-    private String country_name;
+    private String countryName;
 
     /**
      * 身份证信息
      */
     @Column(name = "id_card")
-    private String id_card;
+    private String idCard;
 
     /**
      * 地址
@@ -103,13 +103,13 @@ public class UserDO implements Serializable {
      * 是否启用，0表示否，1表示是
      */
     @Column(name = "bl_enable")
-    private Byte bl_enable;
+    private Byte blEnable;
 
     /**
      * 是否删除，0表示否，1表示是
      */
     @Column(name = "bl_delete")
-    private Byte bl_delete;
+    private Byte blDelete;
 
     /**
      * 版本号
@@ -121,7 +121,7 @@ public class UserDO implements Serializable {
      * 创建时间
      */
     @Column(name = "gmt_create")
-    private LocalDateTime gmt_create;
+    private LocalDateTime gmtCreate;
 
     /**
      * 创建人真实姓名
@@ -133,19 +133,19 @@ public class UserDO implements Serializable {
      * 创建人ID
      */
     @Column(name = "creator_id")
-    private Long creator_id;
+    private Long creatorId;
 
     /**
      * 创建人code
      */
     @Column(name = "creator_code")
-    private String creator_code;
+    private String creatorCode;
 
     /**
      * 修改时间
      */
     @Column(name = "gmt_modify")
-    private LocalDateTime gmt_modify;
+    private LocalDateTime gmtModify;
 
     /**
      * 修改人真实姓名
@@ -157,13 +157,13 @@ public class UserDO implements Serializable {
      * 修改人ID
      */
     @Column(name = "modifier_id")
-    private Long modifier_id;
+    private Long modifierId;
 
     /**
      * 修改人code
      */
     @Column(name = "modifier_code")
-    private String modifier_code;
+    private String modifierCode;
 
     private static final long serialVersionUID = 1L;
 }
--- mybatis-integration/mybatis-integration-mapper/src/test/java/vip/openpark/mapper/mapper/UserDOMapperTest.java
+++ mybatis-integration/mybatis-integration-mapper/src/test/java/vip/openpark/mapper/mapper/UserDOMapperTest.java
@@ -6,8 +6,6 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import tk.mybatis.mapper.mapperhelper.MapperHelper;
 import vip.openpark.mapper.domain.UserDO;
@@ -22,42 +20,24 @@ import java.util.List;
  */
 @Slf4j
 public class UserDOMapperTest {
-	private static SqlSession sqlSession;
-	private static UserDOMapper userDOMapper;
 
-	/**
-	 * junit5 中 @BeforeAll 修饰的必须要是 static 方法。
-	 * 只执行一次，执行时机是在所有 @Test 和 @BeforeEach 注解方法之前
-	 *
-	 * @throws IOException
-	 */
-	@BeforeAll
-	public static void init() throws IOException {
+	@Test
+	public void selectAllTest() throws IOException {
 		Reader reader = Resources.getResourceAsReader("mybatis-config.xml");
 		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
-		sqlSession = sqlSessionFactory.openSession();
+		SqlSession sqlSession = sqlSessionFactory.openSession();
 
 		// 配置通用 mapper
 		MapperHelper mapperHelper = new MapperHelper();
 		mapperHelper.processConfiguration(sqlSession.getConfiguration());
 
-		userDOMapper = sqlSession.getMapper(UserDOMapper.class);
-	}
+		UserDOMapper userDOMapper = sqlSession.getMapper(UserDOMapper.class);
 
-	/**
-	 * junit5 中 @AfterAll 修饰的必须要是 static 方法。
-	 * 只执行一次，执行时机是在所有 @Test 和 @AfterEach 注解方法之后。
-	 */
-	@AfterAll
-	public static void finalExec() {
-		sqlSession.commit();
-		sqlSession.close();
-	}
-
-	@Test
-	public void selectAllTest() {
 		List<UserDO> dataList = userDOMapper.selectAll();
 		log.info("读取到{}记录数", dataList.size());
 		log.info("数据：{}", JSON.toJSONString(dataList));
+
+		sqlSession.commit();
+		sqlSession.close();
 	}
 }
